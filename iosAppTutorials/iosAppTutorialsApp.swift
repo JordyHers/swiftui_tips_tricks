d827dd02f8ac615fe28ee3dbac257c7434e1125c
jordyhers <jordy925@gmail.com>
2021-11-21 23:27:22 +0300

    Initial Commit

---
+++ iosAppTutorials/iosAppTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppTutorialsApp.swift
+//  iosAppTutorials
+//
+//  Created by Jordy Hershel on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct iosAppTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MapkitView()
+        }
+    }
+}
---
+++ iosAppTutorials/views/Mapkit.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iosAppTutorials
+//
+//  Created by Jordy Hershel on 21.11.2021.
+//
+import MapKit
+import SwiftUI
+
+struct MapkitView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 38.856820, longitude: -98.052448), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+    )
+    var body: some View {
+      Map(coordinateRegion: $region)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapkitView()
+    }
+}
