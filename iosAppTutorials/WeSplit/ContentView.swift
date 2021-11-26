17f651802b91c9f7dbf7b820bc5ebd674cd1a294
jordyhers <jordy925@gmail.com>
2021-11-26 20:59:24 +0300

    NavigationBarTitle added

--- iosAppTutorials/WeSplit/ContentView.swift
+++ iosAppTutorials/WeSplit/ContentView.swift
@@ -9,32 +9,22 @@ import SwiftUI
 
 struct ContentViewWeSplit: View {
     var body: some View {
-        /// with swift UI we can now use Form
-        /// Here is the begining of a list of Data as we can see on IOS
-        Form {
-            
-            /// On SWIFTUI we can not have more than 10 children to fix that we need to use Group
-            Group{
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
+        ///The NavigationView needs few parameters such as navigationTitle
+        NavigationView {
+            Form {
+                ///We can use also Section to seperate the parts of the form
+                Section {
+                    Text("Jordy Hers")
+                    Text("Jordy Hers")
+                    Text("Jordy Hers")
+                }
             }
-            ///We can use also Section to seperate the parts of the form
-            Section {
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-            }
-            Group{
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-                Text("Jordy Hers")
-            }
-  
+            ///Make sure to attach the title to the Form and not the navigation View
+            .navigationTitle("SwiftUI")
+            /// We can make the title centered by calling
+            .navigationBarTitleDisplayMode(.inline)
         }
+    
       
     }
 }
---
+++ iosAppTutorials/WeSplit/WeSplitApp.swift
@@ -0,0 +1,18 @@
+//
+//  iosAppTutorialsApp.swift
+//  iosAppTutorials
+//
+//  Created by Jordy Hershel on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+           ContentViewWeSplit()
+            
+        }
+    }
+}
