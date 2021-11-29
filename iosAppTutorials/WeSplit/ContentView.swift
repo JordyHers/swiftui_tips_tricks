f5e5af986457194e8fdf5bd2ed9c169a1e95d051
jordyhers <jordy925@gmail.com>
2021-11-29 21:40:42 +0300

    Adding a segmented control for tip percentages – WeSplit SwiftUI Tutorial 9/11

--- iosAppTutorials/WeSplit/ContentView.swift
+++ iosAppTutorials/WeSplit/ContentView.swift
@@ -8,23 +8,43 @@
 import SwiftUI
 
 struct ContentViewWeSplit: View {
-    let students = ["Jordy","Patrick","Victor","Selena","Berny"]
-    @State private var selectedStudents = "Jordy"
-    
-     
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+     let tipPercentages = [0,5,10,15,20,25]
     var body: some View {
         NavigationView {
             Form {
-                //on SwiftUi there is a Picker like a DropdownButton in Fluttter
-                
-                Picker("Select your student",selection: $selectedStudents) {
-                    ForEach(students, id: \.self){ index in
-                        Text(index)
+                Section {
+                    ///Here Locale is a struct build in SwiftUI that gives us access to locale types of data
+                    TextField("Amount",value: $checkAmount,format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    ///Set up a picker to get the number of people
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
                     }
                 }
-                    
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id:\.self) {
+                            Text($0, format: .percent)
+                        }
+                        //The segmented view gives a better vier in HStack and slidable view
+                    }.pickerStyle(.segmented)
+                } header: {
+                    ///Multiple section allows us to give parameters to the component such as header
+                    Text("How much tip do you want to leave ?   ")
+
                 }
-            }
+                
+                Section {
+                    ///Here Locale is a struct build in SwiftUI that gives us access to locale types of data
+                    Text(checkAmount,format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }.navigationTitle("Tipsy")
+               
         }
        
     }
@@ -34,4 +54,5 @@ struct  ContentViewWeSplit_Previews: PreviewProvider {
     static var previews: some View {
         ContentViewWeSplit()
     }
+}
 }
