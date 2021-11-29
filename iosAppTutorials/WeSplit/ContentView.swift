9dcd639e541d272562de12d96c9c502ef16882ec
jordyhers <jordy925@gmail.com>
2021-11-29 20:54:45 +0300

    Wesplit 155 7/11

--- iosAppTutorials/WeSplit/ContentView.swift
+++ iosAppTutorials/WeSplit/ContentView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct ContentViewWeSplit: View {
-  @State private var tapCount = 0
+    let students = ["Jordy","Patrick","Victor","Selena","Berny"]
+    @State private var selectedStudents = "Jordy"
     
-    
+     
     var body: some View {
-        
-        Button("Tap count: \(tapCount)") {
-            tapCount += 10
+        NavigationView {
+            Form {
+                //on SwiftUi there is a Picker like a DropdownButton in Fluttter
+                
+                Picker("Select your student",selection: $selectedStudents) {
+                    ForEach(students, id: \.self){ index in
+                        Text(index)
+                    }
+                }
+                    
+                }
+            }
         }
-      
+       
     }
-}
+
 
 struct  ContentViewWeSplit_Previews: PreviewProvider {
     static var previews: some View {
