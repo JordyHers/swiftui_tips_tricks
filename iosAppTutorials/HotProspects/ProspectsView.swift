735944e1147d3794d614aec3f85c2cc5d9e0a32b
jordyhers <jordy925@gmail.com>
2021-12-05 23:03:39 +0300

    More Improvements

--- iosAppTutorials/HotProspects/ProspectsView.swift
+++ iosAppTutorials/HotProspects/ProspectsView.swift
@@ -4,13 +4,15 @@
 //
 //  Created by Jordy Hershel on 2.12.2021.
 //
-
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
     
     //Here now we track if the parent struct have an environment object notice that here we write   @EnvironmentObject var Prospects: Prospects not   @EnvironmentObject var Prospects = Prospects()
     @EnvironmentObject var prospects: Prospects
+    
+    @State private var isShowingScanner = false
     
     //In the enum we set up the different cases
     enum FilterType {
@@ -64,20 +66,42 @@ struct ProspectsView: View {
                     .navigationBarTitle(title)
                     .navigationBarItems(trailing: Button(action: {
                         
-                        let prospect = Prospect()
-                        prospect.name = "Jordy Hershel"
-                        prospect.emailAddress = "jordy925@gmail.com"
-                        
-                        self.prospects.people.append(prospect)
+                        self.isShowingScanner = true
                     }) {
                         Image(systemName: "qrcode.viewfinder")
                         Text("Scan")
                     })
+                    .sheet(isPresented: $isShowingScanner) {
+                        
+                        //HEre the simulated data is on purpose seperated by \n
+                        CodeScannerView(codeTypes: [.qr], simulatedData: "Jordy Hershel\njordyhers@email.com", completion: self.handleScan)
+                    }
+        }
+        
+    }
+        func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+            //When the function is triggered the boolean is set to false first
+            self.isShowingScanner = false
             
-         
+            //Then the result are checked: SUCCESS or FAILURE
+            switch result {
+                
+             case .success(let code):
+                //In case of SUCCESS the code inside is seperated by \n
+                let details = code.components(separatedBy: "\n")
+                guard details.count == 2 else { return }
+                
+                let person = Prospect()
+                person.name = details[0]
+                person.emailAddress = details[1]
+                self.prospects.people.append(person)
+
+            case .failure(let error):
+                print("Sanning Failed \(error)")
+            }
         }
     }
-}
+
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
