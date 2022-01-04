e9d7df1f59726308d5a907bcb6efd219534ad29f
jordyhers <jordy925@gmail.com>
2022-01-04 21:26:12 +0300

    121.Adding options with a context menu – Hot Prospects SwiftUI Tutorial 14/16

--- iosAppTutorials/HotProspects/Prospects.swift
+++ iosAppTutorials/HotProspects/Prospects.swift
@@ -18,7 +18,7 @@ class Prospects: ObservableObject {
     //her we initiate the Provider package class then in the Landing page we need to call it	
     @Published var people: [Prospect]
     
-    //Adding options with a context menu – Hot Prospects SwiftUI Tutorial 14/16
+    //121.Adding options with a context menu – Hot Prospects SwiftUI Tutorial 14/16
     let prospect1 = Prospect()
     let prospect2 = Prospect()
     let prospect3 = Prospect()
